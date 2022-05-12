313a8dd9a4846bfc9d217178cb25081a41b9c916
ensenjujin <103574374+ensenjujin@users.noreply.github.com>
2022-05-12 21:56:14 +0800

    接口泪练习1

--- mIdeaTest/src/com/akechiko/demo1/Demo1.java
+++ mIdeaTest/src/com/akechiko/demo1/Demo1.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class Demo1 {
     public static void main(String[] args){
-        BaseTeacher baseTeacher=new BaseTeacher("张三老师",23);
-        System.out.println(baseTeacher.getName()+"....."+baseTeacher.getAge());
-        baseTeacher.teach();
-
-        WorkTeacher workTeacher=new WorkTeacher("李四老师",24);
-        System.out.println(workTeacher.getName()+"....."+workTeacher.getAge());
-        workTeacher.teach();
+        InterIpml ipml=new InterIpml();
+        System.out.println(Inter.num);
+        ipml.method();
     }
 }
+
+interface Inter{
+    int num=10;
+    void method();
+}
+
---
+++ mIdeaTest/src/com/akechiko/demo1/InterIpml.java
@@ -0,0 +1,9 @@
+package com.akechiko.demo1;
+
+public class InterIpml implements Inter{
+
+        @Override
+        public void method() {
+                System.out.println("别打印我");
+        }
+}
