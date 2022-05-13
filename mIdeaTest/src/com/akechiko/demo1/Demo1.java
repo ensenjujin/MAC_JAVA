0e5b530183e61966f5c2d38581c216c4d525f11d
ensenjujin <103574374+ensenjujin@users.noreply.github.com>
2022-05-13 16:20:58 +0800

    多态练习1

--- mIdeaTest/src/com/akechiko/demo1/Demo1.java
+++ mIdeaTest/src/com/akechiko/demo1/Demo1.java
@@ -3,10 +3,29 @@ import java.util.Scanner;
 
 public class Demo1 {
     public static void main(String[] args){
-        NoteBook noteBook=new NoteBook();
-        noteBook.start();
-        noteBook.useUSB(new Mouse());
-        noteBook.stop();
+        Fu fu=new Zi();
+        System.out.println(fu.num);
+        fu.method();
+        Zi zi=(Zi) fu ;
+        zi.show();
+    }
+}
+
+class Fu{
+    int num=10;
+
+    public void method(){
+        System.out.println("父类的method方法");
+    }
+}
+
+class Zi extends Fu{
+    int num=10;
+    public void method(){
+        System.out.println("子类的method方法");
+    }
+    public void show(){
+        System.out.println("子类的show方法");
     }
 }
 
