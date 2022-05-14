ca6a9f8997901342b70176f8d86d55f6ba9a0961
ensenjujin <103574374+ensenjujin@users.noreply.github.com>
2022-05-15 00:20:38 +0800

    时间2

--- mIdeaTest/src/com/akechiko/demo1/Demo1.java
+++ mIdeaTest/src/com/akechiko/demo1/Demo1.java
@@ -1,14 +1,20 @@
 package com.akechiko.demo1;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.logging.SimpleFormatter;
 
 public class Demo1 {
-    public static void main(String[] args){
-        LocalDateTime localDateTime=LocalDateTime.now();
-        DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        System.out.println(localDateTime.format(dateTimeFormatter));
+    public static void main(String[] args) throws ParseException {
+        String str="2018-03-04";
+        SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd");
+        Date now=sf.parse(str);
+        SimpleDateFormat sf1=new SimpleDateFormat("yyyy年MM月dd日");
+        System.out.println(sf1.format(now));
     }
 
 }
