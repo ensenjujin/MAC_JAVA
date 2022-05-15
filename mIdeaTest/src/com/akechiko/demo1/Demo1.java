4a0c46af442727545556a028ff355d254f75f0f3
ensenjujin <103574374+ensenjujin@users.noreply.github.com>
2022-05-15 15:47:48 +0800

    集合1

--- mIdeaTest/src/com/akechiko/demo1/Demo1.java
+++ mIdeaTest/src/com/akechiko/demo1/Demo1.java
@@ -3,16 +3,21 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.SimpleFormatter;
 
 public class Demo1 {
     public static void main(String[] args)  {
-        Student s=new Student();
-        s.setScore(100);
-        s.setScore(-1);
+        Collection <Integer> collections=new ArrayList<>();
+        collections.add(666);
+        collections.add(111);
+        collections.add(222);
+
+        Object[] objects=collections.toArray();
+        for (int i = 0; i < objects.length; i++) {
+            System.out.println(objects[i]);
+        }
+
     }
 
 
