d455e59a8f9030c6540ba428cdd46f3eedee4894
ensenjujin <103574374+ensenjujin@users.noreply.github.com>
2022-05-20 17:42:11 +0800

    UDP发送、接收

--- mIdeaTest/src/com/akechiko/demo1/Demo1.java
+++ mIdeaTest/src/com/akechiko/demo1/Demo1.java
@@ -1,16 +1,21 @@
 package com.akechiko.demo1;
 
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class Demo1 {
-    public static void main(String[] args) throws InterruptedException {
-        ArrayBlockingQueue<String > arrayBlockingQueue=new ArrayBlockingQueue<>(1);
+    public static void main(String[] args) throws IOException {
+        DatagramSocket ds=new DatagramSocket();
+        String s="炸弹";
+        byte[] bytes=s.getBytes();
+        InetAddress address=InetAddress.getByName("127.0.0.1");
+        int port=10000;
+        DatagramPacket dp=new DatagramPacket(bytes,bytes.length,address,port);
 
-        Cooker c=new Cooker(arrayBlockingQueue);
-        Foodie f=new Foodie(arrayBlockingQueue);
+        ds.send(dp);
 
-        c.start();
-        f.start();
-
+        ds.close();
     }
 }
