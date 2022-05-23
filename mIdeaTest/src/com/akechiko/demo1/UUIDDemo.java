4e479597af01e89dd20358525f8ad3d3859c87fb
ensenjujin <103574374+ensenjujin@users.noreply.github.com>
2022-05-23 20:32:29 +0800

    日志slf4j

--- mIdeaTest/src/com/akechiko/demo1/UUIDDemo.java
+++ mIdeaTest/src/com/akechiko/demo1/UUIDDemo.java
@@ -1,11 +1,26 @@
 package com.akechiko.demo1;
 
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.UUID;
 
 public class UUIDDemo {
     public static void main(String[] args) {
-        UUID uuid=UUID.randomUUID();
-        String s=uuid.toString().replace("-","");
-        System.out.println(s);
+        LOGGER.debug("debug级别日志");
+        LOGGER.info("info级别日志");
+        LOGGER.warn("warn级别日志");
+        LOGGER.error("error级别日志");
+    }
+
+    //private static final Logger LOGGER =Logger.getLogger(UUIDDemo.class);
+    private static final Logger LOGGER= LoggerFactory.getLogger(UUIDDemo.class);
+
+    public void sum(){
+        int a=11;
+        int b=22;
+        System.out.println(a+b);
     }
 }
