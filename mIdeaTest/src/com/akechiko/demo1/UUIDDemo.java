d5df36a4bd9f9cb3eb9e48b19bc4098500931099
ensenjujin <103574374+ensenjujin@users.noreply.github.com>
2022-05-24 10:24:38 +0800

    Java基础作业

--- mIdeaTest/src/com/akechiko/demo1/UUIDDemo.java
+++ mIdeaTest/src/com/akechiko/demo1/UUIDDemo.java
@@ -1,26 +1,32 @@
 package com.akechiko.demo1;
 
-
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.UUID;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class UUIDDemo {
-    public static void main(String[] args) {
-        LOGGER.debug("debug级别日志");
-        LOGGER.info("info级别日志");
-        LOGGER.warn("warn级别日志");
-        LOGGER.error("error级别日志");
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new FileReader("mIdeaTest/src/number1.txt"));
+        String line=null;
+        List<String> list=new ArrayList<>();
+        while ((line= br.readLine())!=null) {
+            list.add(line);
+        }
+        br.close();
 
-    //private static final Logger LOGGER =Logger.getLogger(UUIDDemo.class);
-    private static final Logger LOGGER= LoggerFactory.getLogger(UUIDDemo.class);
+        int[] arr=new int[list.size()];
 
-    public void sum(){
-        int a=11;
-        int b=22;
-        System.out.println(a+b);
+        for (int i = 0; i < list.size(); i++) {
+            arr[i]=Integer.parseInt(list.get(i));
+        }
+        Arrays.sort(arr);
+
+        BufferedWriter bw=new BufferedWriter(new FileWriter("mIdeaTest/src/number2.txt"));
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i]+"\n");
+            bw.flush();
+        }
+        bw.close();
     }
 }
